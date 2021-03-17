07b66f027e994cd3595fd0a40a48676215842913
Mau Restor <restor@gmail.com>
2021-03-17 03:07:50 -0600

    Agregando sistema de menus Agregando tres botones en el menu boton de inicio del juego (sin texto, funcional) boton de opciones (sin texto, no funcional) boton salir (sin texto, funcional) Agregando funcionalidades de teclas Esc para entrar al juego principal y mostrar el menu
    Se guarda la posicion y direccion del personaje al salir al menu principal
    
    Registro del tiempo total de nuevo juego (sin funcionalidad total)

--- main.py
+++ main.py
@@ -1,237 +1,246 @@
-import random, pygame, sys, time
+import pygame
+import sys
 from pygame.locals import *
-from pygame import draw
-
 
 pygame.init()
 # Iniciando pygame...
 
+s_width, s_height = (800, 600)
+screen = pygame.display.set_mode((s_width, s_height), pygame.DOUBLEBUF)
 pygame.display.set_caption("Local Market")
 
-## Función principal del juego
+# Fuente
+font = pygame.font.SysFont(None, 14, bold=True)
+clock = pygame.time.Clock()
+fps = 60
+
+count = 0  # Global
+playtime_total = 0.0  # Global
+player_x, player_y = (0, 0)  # Posicion
+direction = 'down'
+
+# Cargar fondo
+fondo = pygame.image.load("img/backgrounds/fondo.png")
+
+# Direccion velocidad del personaje
+player_speed = 4.0
+
+# Declaración de constantes y variables
+WHITE = (255, 255, 255)
+
+key_press = {"left": False,
+             "right": False,
+             "up": False,
+             "down": False,
+             "p_key": False}
+
+
+def draw_text(text, font, color, surface, x, y):
+    """Center text in window"""
+    fw, fh = font.size(text)
+    surface = font.render(text, True, (0, 0, 0), (255, 255, 255, 100))
+    screen.blit(surface, (x, y))
+
+
+def btn_hover(btn, point, color_active, color_hover):
+    ''' Cambia color al pasar el raton sobre el elemento'''
+    collide = btn.collidepoint(point)
+    color = color_hover if collide else color_active
+    pygame.draw.rect(screen, color, btn)
+
+
+def options():
+    global count
+    print("Llamando a opciones: %s :) " % str(count))
+    count += 1
+
+
+def main_menu():
+    ''' Meca Menu inventario
+    TODO
+    Crear un rectangulo HW al 75%
+    Transparentar el rectangulo
+    Dibujar barra superior con titulo
+    distinguir con otro color
+
+    Dibujar pestanias
+    Inventario personaje | opciones
+    '''
+    click = False
+    while True:
+
+        screen.fill((0, 0, 0))
+        draw_text('Menu MK2', font, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 190, 45)
+        button_2 = pygame.Rect(50, 150, 190, 45)
+        button_exit = pygame.Rect(50, 200, 190, 45)
+
+        if button_1.collidepoint((mx, my)):
+            if click:
+                main()
+        if button_2.collidepoint((mx, my)):
+            if click:
+                options()
+        if button_exit.collidepoint((mx, my)):
+            if click:
+                pygame.quit()
+                sys.exit(0)
+
+        # coloreando al pasar
+        point = pygame.mouse.get_pos()
+
+        # pygame.draw.rect(screen, color, button_1)
+        # pygame.draw.rect(screen, color, button_2)
+        btn_hover(button_1, point, (100, 100, 100), (255, 100, 100))
+        btn_hover(button_2, point, (100, 100, 100), (255, 100, 100))
+        btn_hover(button_exit, point, (100, 0, 100), (255, 100, 100))
+
+        click = False
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit(0)
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    main()
+                    # pygame.quit()
+                    # sys.exit(0)
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+            # 3.- Se actualiza la pantalla
+        pygame.display.update()
+        clock.tick(fps)
+
+
+def stats():
+    global playtime_total
+    milliseconds = clock.tick(fps)
+    playtime_total += milliseconds / 500.0
+
+    if key_press['p_key']:
+        draw_text("FPS: {:6.3} Tot time: {:6.3} segs".format(
+                  clock.get_fps(), playtime_total),
+                  font, (255, 255, 255), screen, 20, 20)
+        draw_text("Loc(xy): {}, {}".format(player_x, player_y),
+                  font, (255, 255, 255), screen, 20, 40)
+
+
+class PlayerMeca():
+    '''set controls '''
+
+    def __init__(self, name='', gender='', options=[], active='',
+                 multi=''):
+        pass
+
+    def playerDraw(self, direction='down'):
+        ''' Personaje principal '''
+        player = pygame.image.load('img/pnj/playerSprite.png')
+
+        if direction == 'down':
+            player = player.subsurface(0, 0, 32, 64)
+        elif direction == 'right':
+            player = player.subsurface(32, 0, 32, 64)
+        elif direction == 'up':
+            player = player.subsurface(64, 0, 32, 64)
+        elif direction == 'left':
+            player = player.subsurface(96, 0, 32, 64)
+        return player
+
+
 def main():
+    '''Función principal del juego'''
 
-	# Declaración de constantes y variables
-	WHITE = (255, 255, 255)
-	FPS, fps = 60, 60
+    # Guardar posicion
+    global player_x, player_y
+    global direction
 
-	# resolucion
-	s_width = 800
-	s_heigh = 600
+    player = PlayerMeca()
 
-	#Centro del jugador
-	player_x = s_width/2
-	player_y = s_heigh/2
+    running = True  # Activador del menu
 
-	#Direccion velocidad del personaje
-	player_speed = 4.0
-	direction = 'down'
+    # Bucle principal
+    while running:
 
-	# Cargar fondo
-	fondo = pygame.image.load("img/backgrounds/fondo.png")
-	screen = pygame.display.set_mode((s_width,s_heigh), pygame.DOUBLEBUF)
+        # Mostrar fondo
+        screen.blit(fondo, (0, 0))
 
-	#Fuente 
-	font = pygame.font.SysFont('mono', 14, bold=True)
-	text = font.render("Hello, World", True, (0, 128, 0), (255,255,255))
+        # Se dibuja el personaje
+        screen.blit(player.playerDraw(direction), (player_x, player_y))
 
-	#carga el tiempo
-	start_time = None
-	playtime = 0.0
+        cubo = pygame.Rect(s_width / 2, s_height / 2, 60, 60)
+        pygame.draw.rect(screen, WHITE, cubo)
 
-	clock = pygame.time.Clock()
+        # 2.- Se comprueban los eventos
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == K_ESCAPE:
+                    main_menu()
+                if event.key == K_LEFT or event.key == K_a:
+                    key_press["left"] = True
+                if event.key == K_RIGHT or event.key == K_d:
+                    key_press["right"] = True
+                if event.key == K_UP or event.key == K_w:
+                    key_press["up"] = True
+                if event.key == K_DOWN or event.key == K_s:
+                    key_press["down"] = True
+                if event.key == K_p:  # Comandos FPS
+                    key_press["p_key"] = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == K_a:
+                    key_press["left"] = False
+                if event.key == pygame.K_RIGHT or event.key == K_d:
+                    key_press["right"] = False
+                if event.key == pygame.K_UP or event.key == K_w:
+                    key_press["up"] = False
+                if event.key == pygame.K_DOWN or event.key == K_s:
+                    key_press["down"] = False
+                if event.key == pygame.K_p:
+                    key_press["p_key"] = False
 
-	key_press = {"left": False, "right": False, "up": False, "down": False}
+        if key_press["left"]:  # == Si left es verdadero
+            if player_x <= 0:
+                direction = 'left'
+            else:
+                direction = 'left'
+                player_x -= player_speed
+        if key_press["right"]:
+            if player_x + 32 >= s_width:
+                direction = 'right'
+            else:
+                direction = 'right'
+                player_x += player_speed
+        if key_press["up"]:
+            if player_y <= 0:
+                direction = 'up'
+            else:
+                direction = 'up'
+                player_y -= player_speed
+        if key_press["down"]:
+            if player_y + 265 >= s_width:
+                # Arregla la medida del sprite a 64
+                direction = 'down'
+            else:
+                direction = 'down'
+                player_y += player_speed
 
+        # Estadisticas
+        stats()
 
-	#Mostrando colores en consola
-	for item in pygame.colordict.THECOLORS.items():
-		print(item)
-
-	# Stats de tiempo
-	class Status():
-		def __init__(self):
-			self.last = pygame.time.get_ticks()
-			self.countDown = 1000
-
-		def fire(self, mouse):
-			self.mouse = mouse
-			ahora = pygame.time.get_ticks()
-			if ahora - self.last >= self.countDown:
-				self.last = ahora
-				print(self.mouse)
-
-
-	def doub_press(self, key):
-		''' Al presionar doble una tecla'''
-		self.key = key
-		return "Se presiono dobe la tecla "+self.key
-		pass
-
-
-	class PlayerMeca():
-		''' set controls '''
-		def __init__(self, name='', gender='', options=[], active='', multi=''):
-			# direction = 'down'
-			None
-			
-
-		def playerDraw(self, direction):
-			''' Personaje principal '''
-			player = pygame.image.load('img/pnj/playerSprite.png')
-			
-
-			if direction == 'down':
-				player = player.subsurface(0,0,32,64)
-			elif direction == 'right':
-				player = player.subsurface(32,0,32,64)
-			elif direction == 'up':
-				player = player.subsurface(64,0,32,64)
-			elif direction == 'left':
-				player = player.subsurface(96,0,32,64)
-			return player
-
-
-	# def tiempito(playtime, clock, fps, player_x, player_y):
-	# 	# if start_time:
-	# 	# 	time_since_start = pygame.time.get_ticks() - start_time
-	# 	# 	msg = 'Milisegundos desde el inicio: '+str(time_since_start)
-	# 	# 	screen.blit(font.render(msg, True, (100,100,100)), (30, 30))
-
-	# 	milliseconds = clock.tick(fps)
-	# 	playtime += milliseconds / 1000.0
-	# 	draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS\n\nPosPlayer: {}"
-	# 		.format(clock.get_fps(), " "*5, playtime, str(player_x)+' '+str(player_y)))
+        # 3.- Se actualiza la pantalla
+        pygame.display.update()
+        clock.tick(fps)
 
 
-	def draw_text(text):
-		"""Center text in window"""
-		fw, fh = font.size(text)
-		surface = font.render(text, True, (0, 0, 0))
-		screen.blit(surface, (5,15))
-
-
-	def main_menu():
-		#Meca Menu inventario
-		''' 
-		TODO
-		Crear un rectangulo HW al 75%
-		Transparentar el rectangulo
-		Dibujar barra superior con titulo
-		distinguir con otro color
-
-		Dibujar pestanias
-		Inventario personaje | opciones
-		'''
-		while runing:
-			screen.fill((0,0,0))
-			draw_text('Menu MK2', font, (255,255,255), screen, (20, 20))
-
-			for event in pygame.event.get():
-				if event.type == QUIT:
-					runing=False
-				elif event.type == pygame.KEYDOWN:
-					if event.key == K_ESCAPE:
-						runing = False
-				# 3.- Se actualiza la pantalla
-			pygame.display.update()
-			clock.tick(fps)
-
-		# Salir cuando runing=False
-		pygame.quit()
-		sys.exit(0)
-		
-	
-	# Bucle principal
-	player = PlayerMeca()
-
-	runing = True
-	while runing:
-		
-		# 1.- Se dibuja la pantalla
-		screen.fill(WHITE)
-
-		# Mostrar fondo
-		screen.blit(fondo, (0,0))
-
-		# Se dibuja el personaje
-		screen.blit(player.playerDraw(direction), (player_x, player_y))
-		# screen.blit(menu, s_width, s_heigh)
-		#mouse localizando 
-		mouse = pygame.mouse.get_pos()
-
-		pygame.draw.rect(screen, WHITE, pygame.Rect(s_width/2, s_heigh/2, 60, 60))
-
-		#Dibujando Menu
-		# screen.blit(menu, (0, 0))
-
-		# print("log!" + ' '+direction )
-
-		# 2.- Se comprueban los eventos
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				runing = False
-			elif event.type == pygame.KEYDOWN:
-				if event.key == K_ESCAPE:
-					runing = False
-				if event.key == K_LEFT or event.key == K_a:
-					key_press["left"] = True
-				if event.key == K_RIGHT or event.key == K_d:
-					key_press["right"] = True
-				if event.key == K_UP or event.key == K_w:
-					key_press["up"] = True
-				if event.key == K_DOWN or event.key == K_s:
-					key_press["down"] = True
-			elif event.type == pygame.KEYUP:
-				if event.key == pygame.K_LEFT or event.key == K_a:
-					key_press["left"] = False
-				if event.key == pygame.K_RIGHT or event.key == K_d:
-					key_press["right"] = False
-				if event.key == pygame.K_UP or event.key == K_w:
-					key_press["up"] = False
-				if event.key == pygame.K_DOWN or event.key == K_s:
-					key_press["down"] = False
-
-		if key_press["left"]:# == Si left es verdadero
-			if player_x <= 0:
-				direction = 'left'
-			else:
-				direction = 'left'
-				player_x -= player_speed
-		if key_press["right"]:
-			if  player_x+32 >= s_width:
-				direction = 'right'
-			else:
-				direction = 'right'
-				player_x += player_speed
-		if key_press["up"]:
-			if  player_y <= 0:
-				direction = 'up'
-			else:
-				direction = 'up'
-				player_y -= player_speed
-		if key_press["down"]:
-			if  player_y+265 >= s_width: ## Arregla la medida del sprite para que quede a 64
-				direction = 'down'
-			else:
-				direction = 'down'
-				player_y += player_speed
-
-		# print(mouse)
-		# tiempito(playtime, clock, fps, player_x, player_y)
-		milliseconds = clock.tick(fps)
-		playtime += milliseconds / 500.0
-		draw_text("FPS: {:6.3}{}PLAYTIME: {:6.3} SECONDS\n\nPosPlayer: {}"
-		.format(clock.get_fps(), " "*5, playtime, str(player_x)+' '+str(player_y)))
-
-		# 3.- Se actualiza la pantalla
-		pygame.display.update()
-		clock.tick(fps)
-
-	# Salir cuando runing=False
-	pygame.quit()
-	sys.exit(0)
-
-# Este fichero es el que ejecuta el juego principal
 if __name__ == '__main__':
-	main()
+    # Este fichero es el que ejecuta el juego principal
+    main_menu()
+    # main()
--- menus_prueba.py
+++ menus_prueba.py
@@ -11,6 +11,8 @@ screen = pygame.display.set_mode((500, 500), 0, 32)
 
 fuente = pygame.font.SysFont(None, 20)
 
+color = (255, 255, 255)
+
 
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
@@ -19,26 +21,45 @@ def draw_text(text, font, color, surface, x, y):
     surface.blit(textobj, textrect)
 
 
-click = False
+def btn_hover(btn, point, color_active, color_hover):
+    ''' Cambia color al pasar el raton sobre el elemento'''
+    collide = btn.collidepoint(point)
+    color = color_hover if collide else color_active
+    pygame.draw.rect(screen, color, btn)
 
 
+# click = False
 def main_menu():
+    ''' Menu principal de la aplicacion '''
     while True:
         screen.fill((0, 0, 0))
         draw_text('Menu principal', fuente, (255, 255, 255), screen, 20, 20)
 
         mx, my = pygame.mouse.get_pos()
 
-        button_1 = pygame.Rect(50, 100, 200, 50)
-        button_2 = pygame.Rect(50, 200, 200, 50)
+        button_1 = pygame.Rect(50, 100, 190, 45)
+        button_2 = pygame.Rect(50, 150, 190, 45)
+        button_exit = pygame.Rect(50, 200, 190, 45)
+
         if button_1.collidepoint((mx, my)):
             if click:
                 game()
         if button_2.collidepoint((mx, my)):
             if click:
                 options()
-        pygame.draw.rect(screen, (255, 0, 0), button_1)
-        pygame.draw.rect(screen, (255, 0, 0), button_2)
+        if button_exit.collidepoint((mx, my)):
+            if click:
+                pygame.quit()
+                sys.exit(0)
+
+        #coloreando al pasar
+        point = pygame.mouse.get_pos()
+
+        # pygame.draw.rect(screen, color, button_1)
+        # pygame.draw.rect(screen, color, button_2)
+        btn_hover(button_1, point, (100, 100, 100), (255, 100, 100))
+        btn_hover(button_2, point, (100, 100, 100), (255, 100, 100))
+        btn_hover(button_exit, point, (100, 0, 100), (255, 100, 100))
 
         click = False
 
@@ -60,6 +81,7 @@ def main_menu():
 
 
 def game():
+    ''' Loop principal del juego'''
     running = True
     while running:
         screen.fill((0, 0, 0))
@@ -77,6 +99,7 @@ def game():
 
 
 def options():
+    ''' Menu opciones '''
     running = True
     while running:
         screen.fill((0, 0, 0))
@@ -93,4 +116,5 @@ def options():
         reloj.tick(60)
 
 
-main_menu()
+if __name__ == '__main__':
+    main_menu()
